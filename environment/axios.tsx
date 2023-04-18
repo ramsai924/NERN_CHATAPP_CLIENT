7d05cae93c32b5595748695980c1d00e065710b6
ram.sai.ext@siemens.com <ramsai924@gmail.com>
2023-04-19 01:27:49 +0530

    lastest canges on 19-04-23

--- environment/axios.tsx
+++ environment/axios.tsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const BaseUrl: any = axios.create({
     // baseURL: 'http://localhost:3030',
-    baseURL: 'http://localhost:3030',
+    baseURL: 'nernchatappapi-production.up.railway.app',
 
     withCredentials: true,
     headers: {
